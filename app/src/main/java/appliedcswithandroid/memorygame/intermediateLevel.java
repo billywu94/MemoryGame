36960e56065af49e685ccd248546c6903b4e7871
billywu94 <billy.wu02@myhunter.cuny.edu>
2017-05-08 19:02:53 -0400

    implemented logic for intermediate level

--- app/src/main/java/appliedcswithandroid/memorygame/intermediateLevel.java
+++ app/src/main/java/appliedcswithandroid/memorygame/intermediateLevel.java
@@ -3,8 +3,10 @@ package appliedcswithandroid.memorygame;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +21,7 @@ import java.io.InputStreamReader;
 
 public class intermediateLevel extends Activity{
     private MemoryGame wordContainer;
+    String wordToMemorize = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -38,19 +41,65 @@ public class intermediateLevel extends Activity{
     @Override
     protected void onStart(){
         super.onStart();
-        String wordToMemorize = "";
+        randomWordToMemorize();
+    }
+
+    public void randomWordToMemorize(){
+        String secondWord = wordContainer.chooseRandomIntermediate();
+        String thirdWord = wordContainer.chooseRandomIntermediate();
         wordToMemorize = wordContainer.chooseRandomIntermediate();
+        wordToMemorize = wordToMemorize + " " + secondWord + " " + thirdWord;
+        System.out.println("This are the words: " + wordToMemorize);
         final TextView textView = (TextView) findViewById(R.id.textView2);
         textView.setText(wordToMemorize);
         //source: http://stackoverflow.com/questions/22194761/hide-textview-after-some-time-in-android
+        //http://stackoverflow.com/questions/8177830/hide-a-layout-after-10-seconds-in-android
         textView.postDelayed(new Runnable() {
             @Override
             public void run() {
                 textView.setVisibility(View.INVISIBLE);
             }
-        }, 3000); //set visibility for 3 seconds
+        }, 4000); //set visibility for 3 seconds
 
-        final EditText editText = (EditText) findViewById(R.id.editText1);
+
+    }
+
+    //setting background color
+    //source: http://stackoverflow.com/questions/8961071/android-changing-background-color-of-the-activity-main-view
+    public void setActivityBackgroundColor(int color){
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(color);
+    }
+
+    public void checkWord(View view){
+        Button button = (Button) findViewById(R.id.submitIntermediate);
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        editText.setFocusable(false); //source: http://stackoverflow.com/questions/4297763/disabling-of-edittext-in-android
+        String userInput = editText.getText().toString();
+        System.out.println("This is the user input: " + userInput);
+        if(wordToMemorize.equals(userInput)){
+            button.setEnabled(false);
+            setActivityBackgroundColor(Color.GREEN);
+            Toast.makeText(this,"Congratulations, you got the word correct!!!", Toast.LENGTH_SHORT).show();
+        }else{
+            button.setEnabled(false);
+            setActivityBackgroundColor(Color.RED);
+            Toast.makeText(this,"Sorry, the word you entered was incorrect.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void nextWord(View view){
+        setActivityBackgroundColor(Color.parseColor("#EEEEEE"));
+        TextView textView = (TextView) findViewById(R.id.textView2);
+        textView.setVisibility(View.VISIBLE);
+        randomWordToMemorize();
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        editText.setText("");
+        editText.setFocusableInTouchMode(true);
+        Button button = (Button) findViewById(R.id.submitIntermediate);
+        button.setEnabled(true);
+
     }
 
     //go back to home page
